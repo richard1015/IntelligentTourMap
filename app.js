4bb2505e20b1fb393e964ab809f5b22cd8f827b5
richard <51844712@qq.com>
2019-04-26 17:02:42 +0800

    添加websocket模块，实时日志推送

--- app.js
+++ app.js
@@ -21,25 +21,62 @@ module.exports = app => {
     }
     // 后续数据的更新由定时任务自动触发
     app.logger.info('app cache token reset ');
+    // 缓存数据
     app.cache = { tokens: [] };
+    // websocket缓存链接
+    app.wsClients = new Set();
   });
 
   app.once('server', server => {
     // websocket
+    app.logger.info('web socket started');
+
+    var WebSocketServer = require('ws').Server;
+    var wss = new WebSocketServer({ port: 3344 });
+    wss.on('connection', function connection(ws, req) {
+      ws.on('message', function incoming(message) {
+        const ip = req.connection.remoteAddress;
+        app.logger.info(ip + 'received: %s', message);
+        if (!app.cache.tokens.includes(message)) {
+          app.logger.info('失效token' + message + '已强制关闭该链接！')
+          ws.send('权限不足,请重新登录后台系统！');
+          ws.close();
+        } else {
+          app.logger.info(message + '已连接！')
+          ws.send('已成功连接服务器！');
+          app.wsClients.add(ws)
+        }
+      });
+      ws.on('close', function (code, reason) {
+        this.close();
+        app.logger.info('socket 断开')
+      })
+    });
+    setInterval(() => {
+      app.logger.info('当前后台实时日志在线人数' + app.wsClients.size);
+    }, 1000 * 10);
   });
   app.on('error', (err, ctx) => {
     // report error
   });
   app.on('request', ctx => {
     // log receive request
-    ctx.logger.info('request starttime...', ctx.starttime);
-    ctx.logger.info('request querystring ===', ctx.querystring);
+    // ctx.logger.info('request starttime...', ctx.starttime);
+    // ctx.logger.info('request querystring ===', ctx.querystring);
+    app.wsClients.forEach(ws => {
+      try {
+        ws.send(JSON.stringify(ctx));
+      } catch (error) {
+        ws.close();
+        app.wsClients.delete(ws)
+      }
+    });
   });
   app.on('response', ctx => {
     // ctx.starttime is set by framework
     const used = Date.now() - ctx.starttime;
     // log total cost
-    ctx.logger.info('response endtime...', Date.now());
+    // ctx.logger.info('response endtime...', Date.now());
     ctx.logger.info('response time...', used);
   });
 };
